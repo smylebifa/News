ada07a832bdc201100169cf7409ce9dabb472f5b
Ilia <samoilovilia@mail.ru>
2022-02-24 10:32:06 +0300

    add changes

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -4,4 +4,34 @@
 
 @section('content')
   <h1>Contacts</h1>
+
+  <form action="{{ route ('contact-form') }}" method="post">
+
+    @csrf
+
+    <div class="form-group">
+      <label for="name">Enter name</label>
+      <input type="text" name="name" placeholder="Enter name" id="name" class="form-control">
+    </div>
+
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input type="text" name="email" placeholder="Enter email" id="email" class="form-control">
+    </div>
+
+    <div class="form-group">
+      <label for="subject">Theme of message</label>
+      <input type="text" name="subject" placeholder="Theme of message" id="subject" class="form-control">
+    </div>
+
+    <div class="form-group">
+      <label for="message">Message</label>
+      <textarea name="message" id="message" class="form-control" placeholder="Enter message"></textarea>
+    </div>
+
+    <button type="submit" class="btn btn-success">Send</button>
+
+  </form>  
+
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/about', function () {
   return view('about');
-});
+})->name('about');
 
 Route::get('/contacts', function () {
   return view('contacts');
-});
+})->name('contacts');
+
+Route::post('/contacts/submit', function () {
+  return Request::all();
+})->name('contact-form');
